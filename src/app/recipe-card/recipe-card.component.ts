b071e465090a5852a3cc50af388d0517c97b3c7f
Nour Bsila <nour.bsila@gmail.com>
2025-12-12 00:56:39 +0100

    modified home cards

--- src/app/recipe-card/recipe-card.component.ts
+++ src/app/recipe-card/recipe-card.component.ts
@@ -8,5 +8,8 @@ import { Component, Input } from '@angular/core';
 })
 export class RecipeCardComponent {
   @Input() recipe: any;
+  viewRecipe(id:number): void {
+    console.log('Viewing recipe:', this.recipe);
+  }
   defaultPhoto: string = 'https://images.unsplash.com/photo-1494790108755-2616b786d4d9?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80';
 }
